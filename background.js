4677225af7d158dcffc7e2b1197b22c5b8109f7b
Nikck Chirico <nickchirico07@gmail.com>
2015-11-10 18:00:07 -0500

    removed crap

--- background.js
+++
@@ -1,8 +0,0 @@
-//this background task sends a json to the current tab that just says we pressed the button
-chrome.browserAction.onClicked.addListener(function(tab) {
-	// Send a message to the active tab
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-	});
-});
