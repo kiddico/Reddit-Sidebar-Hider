b7fc8ef8250bd4eb7aaa5e65d6609ee647a429d1
Nikck Chirico <nickchirico07@gmail.com>
2015-11-10 18:36:24 -0500

    got it to work without reloading the page. not sure what to do with all the old code I worked so hard one :P

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 //changes preference in local storage
+//this takes too long, or can just cause weird stuff to happen
+//I got rid of it
 function flipflop(){
 	chrome.storage.local.get(["preference"],function(result){
 		var pref = {};
@@ -14,6 +16,7 @@ function flipflop(){
 		chrome.storage.local.set(dataobj);
 	});
 };
+
 //sends message to event page to reload
 function reloadTheCurrentTab(){
 	console.log("in reload func");
@@ -22,66 +25,47 @@ function reloadTheCurrentTab(){
 	});
 }
 
+//gets rid of saved data
+//if you want to cause yourself a lot of trouble just call it in random places
 function clear(){
 	chrome.storage.local.remove(["preference"]);
 	console.log("cleared");
 }
 
+var sidebarcontents;
 
+////////// Initial Setup
 //Look in the local storage for the preference value
 //action taken depends on state of return
 chrome.storage.local.get(["preference"],function(returned)
 	{
 		var orig = {};
 		orig = returned["preference"];
-		console.log("Initial pref value :" + orig);
 		orig = parseInt(orig);
-		console.log("Initial pref value :" + orig);
 		//I suppose I can put everything in the callback.
 		if(orig == 2){
 			//just chill.
 		}
 		if(orig == 1){
-			console.log("This is in the second orig check");
 			var sidebar = document.querySelector('.side');
+			sidebarcontents = sidebar.innerHTML;
 			sidebar.innerHTML = '';
 			console.log("removed sidebar");
 		}
 		else{
-			console.log("This is in the last orig check");
 			var dataobj = {};
 			dataobj["preference"] = 2;
 			chrome.storage.local.set(dataobj);
 		}
-		chrome.storage.local.get(["preference"],function(returner){
-			console.log("store val after init" + returner["preference"]);
-		});
 	}
 );
 
-
-/*
-//listens for our message from background.js
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		//if the json is actually for us
-		if( request.message === "clicked_browser_action" ) {
-			//this is the actual stuff. it won't be here later.
-			//later this will be a toggle
-			//and the preffered state is saved
-			var sidebar = document.querySelector('.side');
-			sidebar.innerHTML = '';
-			console.log("removed sidebar");
-		}
-	}
-);
-*/
+//Toggle button action
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		//if the message is actually for us
 		if( request.message === "toggle_button_press" ) {
 			//first look at preference
-			console.log("responce in content");
 			chrome.storage.local.get(["preference"],function(result){
 				var pref = {};
 				var dataobj = {};
@@ -89,25 +73,20 @@ chrome.runtime.onMessage.addListener(
 				//convert pref to int so I can actually test against properly
 				var pref = parseInt(pref["preference"]);
 				if(pref == 1){
-					//flipflop();
-					//switched to this in case flipflop takes too long (it will)
 					dataobj["preference"] = 2;
 					chrome.storage.local.set(dataobj);
-					console.log("before reload");
-					reloadTheCurrentTab();
+					var sidebar = document.querySelector('.side');
+					sidebar.innerHTML = sidebarcontents;
 				}
 				else{
-					//flipflop();
 					dataobj["preference"] = 1;
 					chrome.storage.local.set(dataobj);
 					var sidebar = document.querySelector('.side');
+					sidebarcontents = sidebar.innerHTML;
 					sidebar.innerHTML = '';
 					console.log("removed sidebar");
 				}
 			});
-
-			//console.log("pref:" + pref);
-
 		}
 	}
 );
--- eventpage.js
+++ eventpage.js
@@ -1,39 +1,5 @@
-
-/*
-//this background task sends a json to the current tab that just says we pressed the button
-chrome.browserAction.onClicked.addListener(function(tab) {
-	// Send a message to the active tab
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		//var defaultpref = false;
-		//var prefobj["preference"] = defaultpref;
-		//console.log(prefobj);
-		//chrome.storage.local.set(prefobj);
-		
-		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-	});
-});
-*/
-
-//not sure if on startup is for the browser, or just when you hit a page
-/*
-chrome.runtime.onStartup.addListener(function(){
-	console.log("startup");
-});
-
-
-chrome.runtime.onInstalled.addListener(function("install"){};){
-	console.log("in the install bit");
-	var inital = false;
-	var dataobj = {};
-	dataobj["preference"] = initial;
-	//chrome.storage.local.set(initial);
-	console.log("after install bit");
-};
-*/
-
-//okay. so this will have to listen to a message 
-
+//okay. so this will have to listen for a message 
+//if it's the right one it reloads the page
 chrome.runtime.onMessage.addListener(
 		function(request, sender, sendResponse) {
 			console.log(sender.tab ?
@@ -47,5 +13,14 @@ chrome.runtime.onMessage.addListener(
 						chrome.tabs.reload(activetab.id);
 					}
 				);
-				sendResponse({farewell: "goodbye"});
+				sendResponse({farewell: "Page Reloading!"});
 });
+
+//not sure if on startup is for the browser, or just when you hit a page
+/*
+chrome.runtime.onStartup.addListener(function(){
+	console.log("startup");
+});
+*/
+
+
