06fa022fdc16145447f4444b38f6691c31049714
Nikck Chirico <nickchirico07@gmail.com>
2015-11-09 21:41:44 -0500

    added a few comments

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//this background task sends a json to the current tab that just says we pressed the button
 chrome.browserAction.onClicked.addListener(function(tab) {
 	// Send a message to the active tab
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
--- content.js
+++ content.js
@@ -1,9 +1,11 @@
-//gets the list of elements with a class name of side
-//it's an array brah
-
+//listens for our message from background.js
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
+		//if the json is actually for us
 		if( request.message === "clicked_browser_action" ) {
+			//this is the actual stuff. it won't be here later.
+			//later this will be a toggle
+			//and the preffered state is saved
 			var sidebar = document.querySelector('.side');
 			sidebar.innerHTML = '';
 			console.log("removed sidebar");
